1c9aa60e3e97b97f10d338e6f6c3dc54a4088f02
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-19 14:38:54 +0400

    adding constraints into BusinessOwner class

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Models/BusinessOwner.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Models/BusinessOwner.cs
@@ -1,8 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MarketplaceDeliverySystem.Models
 {
+    [Index(nameof(UserId), IsUnique = true)]
+    [Index(nameof(NationalId), IsUnique = true)]
     public class BusinessOwner
     {
         [Key]
@@ -12,5 +15,15 @@ namespace MarketplaceDeliverySystem.Models
         [Required]
         [ForeignKey(nameof(User))]
         public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string NationalId { get; set; }
+
+        [MaxLength(50)]
+        public string? BusinessLicense { get; set; }
+
+        [Required]
+        public DateTime JoinedAt { get; set; }
     }
 }
