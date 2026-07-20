ffd1fcb1b8eadce176740bf5c1e36c3d6f9d20e4
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-20 15:29:11 +0400

    adding DTO for RegDriverDTO

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/RegDriverDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketplaceDeliverySystem.DTOs
+{
+    public class RegDriverDTO
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string LicenseNumber { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(30)]
+        public string VehicleType { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public string VehiclePlateNumber { get; set; } = string.Empty;
+    }
+}
