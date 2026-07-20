e4045966024820a7e14df96bd5226d2c3d69fa9d
Hoor Alwahaibi <hooralwahaibi123@gmail.com>
2026-07-20 15:49:38 +0400

    added RegisterCustomerDTO

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/RegisterCustomerDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketplaceDeliverySystem.DTOs
+{
+    public class RegisterCustomerDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(300)]
+        public string Address { get; set; } = string.Empty;
+    }
+}
