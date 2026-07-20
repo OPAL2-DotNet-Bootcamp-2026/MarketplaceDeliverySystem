85a1fb860c7f47fe1113dba7e267283dab4afe46
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-20 09:35:16 +0400

    add commit

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Models/Delivery.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Models/Delivery.cs
@@ -8,28 +8,37 @@ namespace MarketplaceDeliverySystem.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int DeliveryId { get; set; }
+        //System Generated (Identity Primary Key)
 
         [ForeignKey(nameof(Order))]
         [Required]
         public int OrderId { get; set; }
+        //From List (Selected Order)
 
         public Order Order { get; set; } = null!;
+        //Navigation Property
 
         [ForeignKey(nameof(Driver))]
         [Required]
         public int DriverId { get; set; }
+        //From List (Selected Driver)
 
         public Driver Driver { get; set; } = null!;
+        //Navigation Property
 
         [Required]
         public string DeliveryStatus { get; set; } = string.Empty;
+        //(Default = "Pending" or "Assigned")
 
         [Required]
         public DateTime PickupTime { get; set; }
+        //System Generated (Recorded when the driver picks up the order)
 
         [Required]
         public DateTime DeliveredTime { get; set; }
+        //System Generated (Recorded when the order is delivered)
 
         public decimal DeliveryDuration { get; set; }
+        //Calculated (DeliveredTime - PickupTime)
     }
 }
