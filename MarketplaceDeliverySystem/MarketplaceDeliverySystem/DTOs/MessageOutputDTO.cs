b096905dcf9fabca8195e40b970ba68915f5dcdd
Hoor Alwahaibi <hooralwahaibi123@gmail.com>
2026-07-20 17:42:23 +0400

    added MessageOutputDTO

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/MessageOutputDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MarketplaceDeliverySystem.DTOs
+{
+    public class MessageOutputDTO
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
