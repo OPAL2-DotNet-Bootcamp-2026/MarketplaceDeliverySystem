b12033d96de0cde99f9d7c7a32b613a9bce942c5
hp <hp@DESKTOP-EAQNHGC>
2026-07-21 16:41:03 +0400

    .

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/OrderRepo.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/OrderRepo.cs
@@ -1,6 +1,22 @@
-﻿namespace MarketplaceDeliverySystem.Repos
+﻿using MarketplaceDeliverySystem.Models;
+
+namespace MarketplaceDeliverySystem.Repos
 {
     public class OrderRepo
     {
+        private readonly MarketplaceContext _context;
+
+        public OrderRepo(MarketplaceContext context)
+        {
+            _context = context;
+        }
+
+        public void AddOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+        
+      
     }
 }
