b9cf51716d8ca41578dde651f97f12539786f91f
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-21 15:12:29 +0400

    add unique constraint for the business email

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Models/Business.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Models/Business.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MarketplaceDeliverySystem.Models
 {
+    [Index(nameof(Email), IsUnique = true)]
     public class Business
     {
         [Key]
