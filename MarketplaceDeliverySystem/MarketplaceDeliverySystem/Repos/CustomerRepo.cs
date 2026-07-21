bd951b71f4d222861c78871f50be36265aec7e6c
hooralwahaibi-ai <hooralwahaibi123@gmail.com>
2026-07-22 00:07:20 +0400

    added CustomerRepo

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/CustomerRepo.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/CustomerRepo.cs
@@ -1,6 +1,20 @@
-﻿namespace MarketplaceDeliverySystem.Repos
+﻿using MarketplaceDeliverySystem.Models;
+
+namespace MarketplaceDeliverySystem.Repos
 {
     public class CustomerRepo
     {
+        private readonly MarketplaceContext _context;
+
+        public CustomerRepo(MarketplaceContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
     }
 }
