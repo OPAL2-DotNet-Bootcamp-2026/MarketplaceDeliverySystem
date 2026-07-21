9c0117fd72897649c190b67ceed9849566a5b002
hp <hp@DESKTOP-EAQNHGC>
2026-07-21 10:49:46 +0400

    add repo

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/DriverRepo.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/DriverRepo.cs
@@ -1,6 +1,38 @@
-﻿namespace MarketplaceDeliverySystem.Repos
+﻿using MarketplaceDeliverySystem.Models;
+
+namespace MarketplaceDeliverySystem.Repos
 {
-    public class DriverRepo
-    {
+
+        public class DriverRepo
+        {
+            private MarketplaceContext context;
+
+            public DriverRepo(MarketplaceContext _context)
+            {
+                context = _context;
+            }
+
+            public void AddDriver(Driver driver)
+            {
+                context.Drivers.Add(driver);
+                context.SaveChanges();
+            }
+
+            public Driver? GetDriverByLicenseNumber(string licenseNumber)
+            {
+                return context.Drivers
+                    .FirstOrDefault(d => d.LicenseNumber == licenseNumber);
+            }
+
+            public Driver? GetDriverByVehiclePlate(string plateNumber)
+            {
+                return context.Drivers
+                    .FirstOrDefault(d => d.VehiclePlateNumber == plateNumber);
+            }
+
+            public void Save()
+            {
+                context.SaveChanges();
+            }
+        }
     }
-}
