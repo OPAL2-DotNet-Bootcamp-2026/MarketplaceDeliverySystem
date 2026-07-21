ddd951a6c51740f62bc7a006d57ab3fdbdaa0e85
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-21 11:09:03 +0400

    Creating business reg dto

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/BusinessRegInputDTO.cs
@@ -0,0 +1,36 @@
+﻿using MarketplaceDeliverySystem.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarketplaceDeliverySystem.DTOs
+{
+    public class BusinessRegInputDTO
+    { 
+
+        [Required(ErrorMessage = "Business name is required.")]
+        [MaxLength(100)]
+        public string BusinessName { get; set; }//User Input
+
+        [MaxLength(500)]
+        public string? Description { get; set; }//User Input
+
+        [MaxLength(300)]
+        public string? LogoUrl { get; set; }//User Input
+
+        [Required(ErrorMessage = "Business email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid business email address.")]
+        [MaxLength(100)]
+        public string Email { get; set; }//user input
+
+        [Required(ErrorMessage = "Business address is required.")]
+        [MaxLength(200)]
+        public string Address { get; set; }//user input
+
+        [Required]
+        public TimeOnly OpeningTime { get; set; }//user input
+
+        [Required]
+        public TimeOnly ClosingTime { get; set; }//user input
+
+    }
+}
