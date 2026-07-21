ba98807102bb80d0f26077858ea36945007ec5a1
hp <hp@DESKTOP-EAQNHGC>
2026-07-21 16:09:04 +0400

    add rep

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/CustomerRepo.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/CustomerRepo.cs
@@ -1,6 +1,26 @@
-﻿namespace MarketplaceDeliverySystem.Repos
+﻿using MarketplaceDeliverySystem.Models;
+
+namespace MarketplaceDeliverySystem.Repos
 {
     public class CustomerRepo
     {
+        private MarketplaceContext context;
+
+        public CustomerRepo(MarketplaceContext _context)
+        {
+            context = _context;
+        }
+
+        public Customer GetCustomerById(int customerId)
+        {
+            return context.Customers
+                .FirstOrDefault(c => c.CustomerId == customerId);
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
     }
 }
