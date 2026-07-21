053dcbe5ede13d2a32acd461ebf2eeead85abdf2
hooralwahaibi-ai <hooralwahaibi123@gmail.com>
2026-07-22 00:14:25 +0400

    added RegisterCustomerOutputDTO

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/RegisterCustomerOutputDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace MarketplaceDeliverySystem.DTOs
+{
+    public class RegisterCustomerOutputDTO
+    {
+        public int CustomerId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Role { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime ExpiresAtUtc { get; set; }
+    }
+}
