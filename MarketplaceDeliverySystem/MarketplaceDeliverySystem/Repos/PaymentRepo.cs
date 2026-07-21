14d31fb788039c15ca8e333fef2424989c6ce5db
hp <hp@DESKTOP-EAQNHGC>
2026-07-21 16:40:44 +0400

    .

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/PaymentRepo.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/PaymentRepo.cs
@@ -1,6 +1,20 @@
-﻿namespace MarketplaceDeliverySystem.Repos
+﻿using MarketplaceDeliverySystem.Models;
+
+namespace MarketplaceDeliverySystem.Repos
 {
     public class PaymentRepo
     {
+        private readonly MarketplaceContext _context;
+
+        public PaymentRepo(MarketplaceContext context)
+        {
+            _context = context;
+        }
+
+        public void AddPayment(Payment payment)
+        {
+            _context.Payments.Add(payment);
+            _context.SaveChanges();
+        }
     }
 }
