106d0c167a4e4d257d8a62ece68ea003553cca80
hp <hp@DESKTOP-EAQNHGC>
2026-07-21 15:52:08 +0400

    .

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/OrderItemCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketplaceDeliverySystem.DTOs
+{
+    public class OrderItemCreateDTO
+    {
+        [Range(1, int.MaxValue)]
+        public int ProductId { get; set; }
+
+        [Range(1, 999)]
+        public int Quantity { get; set; }
+    }
+}
