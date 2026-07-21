453c28b9229a35293a2432fc36068afc2a248281
alzulaikha <alzulaikha1992@gmail.com>
2026-07-21 16:39:50 +0400

    added RegisterBusinessOwner

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/FilterProductsOutputDto.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/FilterProductsOutputDto.cs
@@ -49,32 +49,5 @@ namespace MarketplaceDeliverySystem.DTOs
 
 
 
-
-
-
-
-
-
-
-
-        public int ProductId { get; set; }
-
-        public string ProductName { get; set; }
-
-        public string? Description { get; set; }
-
-        public decimal Price { get; set; }
-
-        public int StockQuantity { get; set; }
-
-        public bool IsAvailable { get; set; }
-
-        public string? ImageUrl { get; set; }
-
-        public string BusinessName { get; set; }
-
-        public string CategoryName { get; set; }
-
-        public double AverageRating { get; set; }
     }
 }
--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/BusinessOwnerRepo.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/BusinessOwnerRepo.cs
@@ -1,6 +1,32 @@
-﻿namespace MarketplaceDeliverySystem.Repos
+﻿using MarketplaceDeliverySystem.DTOs;
+using MarketplaceDeliverySystem.Models;
+
+namespace MarketplaceDeliverySystem.Repos
 {
     public class BusinessOwnerRepo
     {
+
+
+        private readonly MarketplaceContext context;
+
+        public BusinessOwnerRepo(MarketplaceContext _context)
+        {
+            context = _context;
+        }
+
+        public bool EmailExists(string email)
+        {
+            return context.Users.Any(u => u.Email == email);
+        }
+        public bool NationalIdExists(string nationalId)
+        {
+            return context.BusinessOwners.Any(b => b.NationalId == nationalId);
+        }
+        public void Add(BusinessOwner owner)
+        {
+            context.BusinessOwners.Add(owner);
+            context.SaveChanges();
+        }
+
     }
 }
