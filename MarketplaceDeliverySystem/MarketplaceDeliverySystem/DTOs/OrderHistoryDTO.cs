43283b8af025c6c6dd51ecb2f3192bd61256f33f
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-22 15:55:11 +0400

    creating response dtos serving View Customer order history

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/OrderHistoryDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace MarketplaceDeliverySystem.DTOs
+{
+    public class OrderHistoryDTO
+    {
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public string OrderStatus { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string PaymentStatus { get; set; }
+
+        public string DeliveryStatus { get; set; }
+
+        public List<OrderItemHistoryDTO> Products { get; set; }
+    }
+}
---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/OrderItemHistoryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MarketplaceDeliverySystem.DTOs
+{
+    public class OrderItemHistoryDTO
+    {
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+}
