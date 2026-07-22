d8500c046990dd3e15041d8083ececd2c1b5b797
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-22 09:02:34 +0400

    get category id to implement update product info function

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/CategoryRepo.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Repos/CategoryRepo.cs
@@ -1,6 +1,20 @@
-﻿namespace MarketplaceDeliverySystem.Repos
+﻿using MarketplaceDeliverySystem.Models;
+
+namespace MarketplaceDeliverySystem.Repos
 {
     public class CategoryRepo
     {
+        private readonly MarketplaceContext _context;
+
+        public CategoryRepo(MarketplaceContext context)
+        {
+            _context = context;
+        }
+
+        public Category? GetById(int categoryId)
+        {
+            return _context.Categories
+                           .FirstOrDefault(c => c.CategoryId == categoryId);
+        }
     }
 }
