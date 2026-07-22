f4bdbaac01cc75c51556a426f288d7e9c42be323
hp <hp@DESKTOP-EAQNHGC>
2026-07-22 10:46:06 +0400

    add Controllers for admin

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using MarketplaceDeliverySystem.DTOs;
+using MarketplaceDeliverySystem.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketplaceDeliverySystem.Controllers
+{
+    [ApiController]
+    [Route("statistics")]
+    [Authorize(Roles = "Admin")]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly AdminService _adminService;
+
+        public StatisticsController(AdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpGet("marketplace")]
+        public IActionResult GetMarketplaceStatistics()
+        {
+            MarketplaceStatisticsOutputDTO result =
+                _adminService.GetMarketplaceStatistics();
+
+            return Ok(result);
+        }
+    }
+}
