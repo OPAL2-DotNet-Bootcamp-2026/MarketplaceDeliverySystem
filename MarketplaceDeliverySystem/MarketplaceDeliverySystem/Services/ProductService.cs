3328ec088ed732cbd48c0a7bd677fbded9ef7178
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-22 09:01:44 +0400

    working on update product info service

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Services/ProductService.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Services/ProductService.cs
@@ -1,6 +1,42 @@
-﻿namespace MarketplaceDeliverySystem.Services
+﻿using MarketplaceDeliverySystem.Controllers;
+using MarketplaceDeliverySystem.DTOs;
+using MarketplaceDeliverySystem.Repos;
+
+namespace MarketplaceDeliverySystem.Services
 {
     public class ProductService
     {
+        private readonly ProductRepo _productRepository;
+        private readonly CategoryRepo _categoryRepository;
+
+        public ProductService(ProductRepo productRepository, CategoryRepo categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public void UpdateProduct(UpdateProductDTO dto)
+        {
+            // Check product exists
+            var product = _productRepository.GetById(dto.ProductId);
+
+            if (product == null)
+                throw new Exception("Product not found.");
+
+            // Check category exists
+            var category = _categoryRepository.GetById(dto.CategoryId);
+
+            if (category == null)
+                throw new Exception("Category not found.");
+
+            // Update product
+            product.ProductName = dto.ProductName;
+            product.Description = dto.Description;
+            product.Price = dto.Price;
+            product.StockQuantity = dto.StockQuantity;
+            product.CategoryId = dto.CategoryId;
+
+            _productRepository.Update(product);
+        }
     }
 }
