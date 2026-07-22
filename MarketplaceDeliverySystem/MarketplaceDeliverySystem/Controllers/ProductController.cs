ac713a6fd7617ced61b9fe8c04ee7743964949b0
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-22 09:00:37 +0400

    update product info controller is created

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Controllers/ProductController.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Controllers/ProductController.cs
@@ -1,6 +1,26 @@
-﻿namespace MarketplaceDeliverySystem.Controllers
+﻿using MarketplaceDeliverySystem.DTOs;
+using MarketplaceDeliverySystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketplaceDeliverySystem.Controllers
 {
-    public class ProductController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
     {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPut("update")]
+        public IActionResult UpdateProduct(ProductUpdateDTO dto)
+        {
+            _productService.UpdateProduct(dto);
+
+            return Ok("Product updated successfully.");
+        }
     }
 }
