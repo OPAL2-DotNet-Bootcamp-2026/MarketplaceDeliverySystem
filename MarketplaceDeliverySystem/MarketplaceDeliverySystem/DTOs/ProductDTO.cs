6d8807332983f618072f7fde00cf2196e0bb4fcf
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-23 10:39:00 +0400

    product response dto of View business with their products

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketplaceDeliverySystem.DTOs
+{
+    public class BusinessProductRespDTO
+    {
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public double AverageRating { get; set; }
+    }
+}
