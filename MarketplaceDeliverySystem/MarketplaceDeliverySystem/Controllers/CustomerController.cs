eaf709653298fe053a8df8b2810ed63b8987f783
hooralwahaibi-ai <hooralwahaibi123@gmail.com>
2026-07-24 01:57:35 +0400

    added customer controller

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Controllers/CustomerController.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Controllers/CustomerController.cs
@@ -1,6 +1,36 @@
-﻿namespace MarketplaceDeliverySystem.Controllers
+﻿using MarketplaceDeliverySystem.DTOs;
+using MarketplaceDeliverySystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketplaceDeliverySystem.Controllers
 {
-    public class CustomerController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
     {
+        private readonly CustomerService _customerService;
+
+        public CustomerController(
+            CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost("Register")]
+        public IActionResult Register(
+            [FromBody] RegisterCustomerDTO dto)
+        {
+            try
+            {
+                RegisterCustomerOutputDTO result =
+                    _customerService.RegisterCustomer(dto);
+
+                return Ok(result);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
     }
 }
