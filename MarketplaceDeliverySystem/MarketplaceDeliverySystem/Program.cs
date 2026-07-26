1bd731db6d0b6883c4fb9345b5c5841791e1bc1d
alzulaikha <alzulaikha1992@gmail.com>
2026-07-26 15:54:06 +0400

    swagger

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Program.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Program.cs
@@ -4,6 +4,7 @@ using MarketplaceDeliverySystem.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace MarketplaceDeliverySystem
@@ -121,24 +122,59 @@ namespace MarketplaceDeliverySystem
             builder.Services.AddAuthorization();
 
             // =====================================================
-            // 6. REGISTER CONTROLLERS AND OPENAPI
+            // 6. REGISTER CONTROLLERS AND SWAGGER
             // =====================================================
 
             builder.Services.AddControllers();
 
-            builder.Services.AddOpenApi();
+            // ??- Swagger with JWT support ???
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your JWT token in the box below"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id   = "Bearer"
+                }
+            },
+            new List<string>()
+        }
+    });
+            });
+
+
+   
 
             // All service registrations must be above Build().
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline / middleware pipeline
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
             // =====================================================
             // 7. CONFIGURE HTTP PIPELINE
             // =====================================================
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
 
             app.UseHttpsRedirection();
 
