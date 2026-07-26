e016323663e3ffa351bd4c5c25f90031d1bca0e4
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-26 14:06:02 +0400

    Adding DTOs of getOrderById() function

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/OrderDetailsDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace MarketplaceDeliverySystem.DTOs
+{
+    public class OrderDetailsDTO
+    {
+        public int OrderId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string BusinessName { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public string OrderStatus { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string DeliveryAddress { get; set; }
+
+        public List<OrderItemDTO> Products { get; set; } = new();
+    }
+}
