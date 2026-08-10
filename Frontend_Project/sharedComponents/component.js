351f823d1acc8ea9f13fa56c3a21be6ee68d3a77
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-08-10 14:51:15 +0400

    done with footer as a shared component

---
+++ Frontend_Project/sharedComponents/component.js
@@ -0,0 +1,12 @@
+
+// Go and get footer.html
+fetch("sharedComponents/footer.html")
+    .then(response => response.text())  //Read the HTML inside that file as text.
+    .then(data => {
+        // document.getElementById("footer-container") will find <div id="footer-container"></div>
+        document.getElementById("footer-container").innerHTML = data;
+        // .innerHTML: Put the footer HTML inside that container.
+        //document. represents the entire HTML of the webpage
+    });
+
+    
