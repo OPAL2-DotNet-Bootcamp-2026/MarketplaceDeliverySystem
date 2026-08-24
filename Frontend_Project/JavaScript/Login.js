3d722d217fdc1c36962941aecf57c56718915ce1
MohammedAlkahali <mohammedharib21@gmail.com>
2026-08-24 17:23:17 +0400

    Send login credentials to backend API

--- Frontend_Project/JavaScript/Login.js
+++ Frontend_Project/JavaScript/Login.js
@@ -1,10 +1,21 @@
-const loginForm = document.getElementById('loginForm');
-
-loginForm.addEventListener('submit', function (event) {
+loginForm.addEventListener('submit', async function (event) {
   event.preventDefault();
 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  console.log(email, password);
+  const loginData = {
+    email: email,
+    password: password
+  };
+
+  const response = await fetch('https://localhost:7299/api/User/Login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(loginData)
+  });
+
+  console.log(response);
 });
