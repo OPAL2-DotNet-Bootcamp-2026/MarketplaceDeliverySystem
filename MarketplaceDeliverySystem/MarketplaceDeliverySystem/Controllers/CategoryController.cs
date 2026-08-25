aa61b3400bc34edd9aa810d7940a1d207be78ec9
MrSn001 <shaheen36.saa@gmail.com>
2026-08-25 11:04:31 +0400

    Implementing CategoryController

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Controllers/CategoryController.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Controllers/CategoryController.cs
@@ -1,6 +1,26 @@
-﻿namespace MarketplaceDeliverySystem.Controllers
+﻿using MarketplaceDeliverySystem.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketplaceDeliverySystem.Controllers
 {
-    public class CategoryController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
     {
+        private readonly CategoryService _categoryService;
+
+        public CategoryController(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("GetSidebarCategories")]
+        [AllowAnonymous]
+        public IActionResult GetSidebarCategories()
+        {
+            var result = _categoryService.GetSidebarCategories();
+            return Ok(result);
+        }
     }
 }
