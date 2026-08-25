80871c7752db02eb2c889c7e90de0068279aedda
MrSn001 <shaheen36.saa@gmail.com>
2026-08-25 10:48:48 +0400

    Implementing the CategorySidebarDTO DTO

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/DTOs/CategorySidebarDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MarketplaceDeliverySystem.DTOs
+{
+    public class CategorySidebarDTO
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
