1b5b3e3d586d3e38d6381307f3d92f715d0c80dc
MrSn001 <shaheen36.saa@gmail.com>
2026-09-15 15:51:43 +0400

    Adding the BusinessCategory Properties

--- MarketplaceDeliverySystem/MarketplaceDeliverySystem/Models/BusinessCategory.cs
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Models/BusinessCategory.cs
@@ -1,6 +1,20 @@
-﻿namespace MarketplaceDeliverySystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarketplaceDeliverySystem.Models
 {
     public class BusinessCategory
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BusinessCategoryId { get; set; } //System Generated 
+
+        [Required]
+        public string BusinessCategoryName { get; set; } //User Input
+
+        [Url]
+        public string? BusinessCategoryImageURL { get; set; } //User Input
+
+        public ICollection<Business> businesses { get; set; } = new List<Business>(); //navigation properties
     }
 }
