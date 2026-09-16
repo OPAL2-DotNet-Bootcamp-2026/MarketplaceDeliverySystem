545f68386f03e1f67de9c8749d7b1051930d063e
MrSn001 <shaheen36.saa@gmail.com>
2026-09-16 20:13:57 +0400

    Implementing the BusinessCategoryController

---
+++ MarketplaceDeliverySystem/MarketplaceDeliverySystem/Controllers/BusinessCategoryController.cs
@@ -0,0 +1,28 @@
+﻿using MarketplaceDeliverySystem.DTOs;
+using MarketplaceDeliverySystem.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketplaceDeliverySystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BusinessCategoryController : ControllerBase
+    {
+        private readonly BusinessCategoryService _businessCategoryService;
+
+        public BusinessCategoryController(BusinessCategoryService businessCategoryService)
+        {
+            _businessCategoryService = businessCategoryService;
+        }
+
+        [HttpGet("GetSidebarCategories")]
+        [AllowAnonymous]
+        public IActionResult GetSidebarCategories()
+        {
+            var result = _businessCategoryService.GetSidebarCategories();
+            return Ok(result);
+        }
+
+    }
+}
