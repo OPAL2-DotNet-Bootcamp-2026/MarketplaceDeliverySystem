2df861d18a9b7ec25108dbf209c507126eeff5b9
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-09-23 11:12:48 +0400

    header and sidebar

--- AngularVersion/MarketPlaceAngular/src/app/app.ts
+++ AngularVersion/MarketPlaceAngular/src/app/app.ts
@@ -1,13 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { Sidebar } from './layout/sidebar/sidebar';
 import { Footer } from './layout/footer/footer';
 
 @Component({
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Header, Sidebar, Footer],
   selector: 'app-root',
-  styleUrl: './app.css',
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('MarketPlaceAngular');
+
+  isSidebarOpen = false;
+
+  openSidebar(): void {
+    this.isSidebarOpen = true;
+  }
+
+  closeSidebar(): void {
+    this.isSidebarOpen = false;
+  }
 }
